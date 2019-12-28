8e8241cb6f83194aa371efbf4f4e8f84deb6be50
behnasr <m.m.behnasr.2000@gmail.com>
2019-12-28 16:12:38 +0330

    start

---
+++ music/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+# red pk 1
+
+# primary key && foreign key
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+
+# model.CASCADE dare mige age  album hazf shod song ham hazf beshe
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
---
+++ music/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    # /music/
+    url(r'^$', views.index, name='index'),
+
+    # /music/712/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+]
